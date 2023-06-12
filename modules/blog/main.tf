b7d421516fd47ec3447f4c2bf6e338e406344da1
Armando Lara <31377858+armandolara24@users.noreply.github.com>
2023-06-11 21:58:15 -0600

    Update main.tf

--- modules/blog/main.tf
+++ modules/blog/main.tf
@@ -22,6 +22,7 @@ module "blog_vpc" {
   cidr = "${var.environment.network_prefix}.0.0/16"
 
   azs             = ["us-west-2a","us-west-2b","us-west-2c"]
+  private_subnets  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["${var.environment.network_prefix}.101.0/24", "${var.environment.network_prefix}.102.0/24", "${var.environment.network_prefix}.103.0/24"]
 
   enable_nat_gateway = true
